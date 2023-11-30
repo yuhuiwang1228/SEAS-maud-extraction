bd25be4a4634da973197d857b8ca904d6a9e74cd
yuhuiwang1228 <1670594368@qq.com>
2023-11-29 21:55:57 -0500

    update train

--- run_maud_best_hp_new.sh
+++ run_maud_best_hp_new.sh
@@ -5,10 +5,13 @@ for run_num in 1; do
   for epoch_num in 4; do
     for lr in 1e-4; do
       output_dir=./train_models/test_${cache_type}/roberta-base-maud-lr-$lr
+      model_dir=./train_models/test_${cache_type}/roberta-base-maud-lr-$lr
       predict_dir=./train_models/test_${cache_type}/predict/train_126_seg_uniq
       predict_file=./maud_data/maud_squad_${cache_type}_answers/maud_squad_train_126_seg_uniq.json
+      python -m torch.distributed.launch --nproc_per_node=4
       python train.py \
               --output_dir $output_dir \
+              --model_dir $model_dir \
               --model_type roberta \
               --model_name_or_path roberta-base \
               --train_file ./maud_data/maud_squad_${cache_type}_answers/maud_squad_train_and_dev.json \
@@ -18,7 +21,7 @@ for run_num in 1; do
               --learning_rate $lr \
               --num_train_epochs ${epoch_num} \
               --per_gpu_eval_batch_size=16  \
-              --per_gpu_train_batch_size=64 \
+              --per_gpu_train_batch_size=32 \
               --max_seq_length 512 \
               --max_answer_length 512 \
               --doc_stride 256 \
@@ -27,7 +30,9 @@ for run_num in 1; do
               --threads 6 \
               --do_train \
               --do_eval \
-              --n_best_size 100
+              --do_freeze \
+              --n_best_size 10
+      
       python evaluate.py -E test -T $predict_file $predict_dir
     done
   done
